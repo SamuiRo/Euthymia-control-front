e625497ae986ee3f6f941c4f01d080234f7484ca
StarLith <hommerclod@gmail.com>
2024-06-30 15:56:12 +0300

    v0.6.4 update activity

--- src/app/components/web3/sub-menu/arcade/arcade-overview/arcade-overview.component.ts
+++ src/app/components/web3/sub-menu/arcade/arcade-overview/arcade-overview.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { MarkdownModule } from 'ngx-markdown';
+import { ActivityService } from '../../../../../services/activity/activity.service';
 
 @Component({
   selector: 'app-arcade-overview',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MarkdownModule],
   templateUrl: './arcade-overview.component.html',
-  styleUrl: './arcade-overview.component.sass'
+  styleUrl: './arcade-overview.component.sass',
 })
 export class ArcadeOverviewComponent {
+  activity_name: string = '';
+  article_content: string = '';
 
+  constructor(
+    private route: ActivatedRoute,
+    private ActivityService: ActivityService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.activity_name = params['activity']; // "+" перед params['id'] конвертує його в число
+      // console.log(name); // Робіть тут, що завгодно з отриманим id
+    });
+
+    this.getArcadeOverview(this.activity_name);
+  }
+
+  getArcadeOverview(name: string) {
+    this.ActivityService.get_arcade_overview(name).subscribe({
+      next: (value) => {
+        console.log(value);
+        this.article_content = value.data.details.article_text;
+      },
+      error: (error) => alert(error.message),
+      complete: () => console.log('complete'),
+    });
+  }
 }
--- src/app/components/web3/sub-menu/arcade/arcade.component.ts
+++ src/app/components/web3/sub-menu/arcade/arcade.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { ActivityService } from '../../../../services/activity/activity.service';
+import { NotificationService } from '../../../../services/notification/notification.service';
 
 @Component({
   selector: 'app-arcade',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './arcade.component.html',
-  styleUrl: './arcade.component.sass'
+  styleUrl: './arcade.component.sass',
 })
-export class ArcadeComponent {
+export class ArcadeComponent implements OnInit {
+  arcade_list: any;
 
+  constructor(
+    private activityService: ActivityService,
+    private notificationService: NotificationService
+  ) {}
+
+  ngOnInit() {
+    // this.getRoutineList();
+    this.getArcadeList();
+  }
+
+  getArcadeList() {
+    this.activityService.get_arcade_list().subscribe({
+      next: (value) => {
+        console.log(value);
+        this.arcade_list = value.data.rows;
+        this.notificationService.showNotification(value.message, 'gold', 2000);
+      },
+      error: (error) => {
+        this.notificationService.showNotification(error.message, 'gold', 60000);
+      },
+      complete: () => console.log('complete'),
+    });
+  }
+
+  getTierClass(tier: number): string {
+    switch (tier) {
+      case 1:
+        return 'tier-1';
+      case 2:
+        return 'tier-2';
+      case 3:
+        return 'tier-3';
+      case 4:
+        return 'tier-4';
+      default:
+        return 'default';
+    }
+  }
 }
